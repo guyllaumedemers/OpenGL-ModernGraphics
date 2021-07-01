e02fd4c5aa120e8f0b8555807676ae2ab1b4b3ac
guyllaume demers <guyllaume_demers@hotmail.fr>
2021-07-01 19:55:40 -0400

    Pushing fix for openGL template - main
    function calls are moved inside the utilities namespace

--- Plain_Red_Cube/Plain_Red_Cube/Plain_Red_Cube/Main.cpp
+++ Plain_Red_Cube/Plain_Red_Cube/Plain_Red_Cube/Main.cpp
@@ -15,92 +15,32 @@
 #define numVAOs 1
 #define numVBOs 2
 
-GLuint renderingProgram;
+GLuint renProg;
 GLuint vao[numVAOs];
 GLuint vbo[numVBOs];
 
 float vertexArr[108] = {};
 
 glm::mat4 mvMat, perpMat;
+glm::vec3 cam, model;
+GLuint mvLoc, projLoc;
 
-GLFWwindow* CreateWindow(const float& width, const float& height, const char* name, const float& major_version, const float& minor_version) {
-	if (!glfwInit()) {
-		std::puts("GLFW lib not initialize");
-		exit(EXIT_FAILURE);
-	}
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major_version);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor_version);
+const char* vp = "vp.shader";
+const char* fp = "fp.shader";
 
-	return glfwCreateWindow(width, height, name, NULL, NULL);
-}
+const char* mv_matrix = "mv_matrix";
+const char* proj_matrix = "proj_matrix";
 
-void GameLoop(GLFWwindow* window, const char* vp, const char* fp, float model[], void (*init)(GLFWwindow* window, const char* vp, const char* fp, float model[]),
-	void (*display)(GLFWwindow* window, double currentTime))
-{
-	glfwSwapInterval(1);
-	init(window, vp, fp, model);
-	while (!glfwWindowShouldClose(window)) {
-		display(window, glfwGetTime());
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-}
-
-void DestroyWindow(GLFWwindow* window) {
-	glfwDestroyWindow(window);
-	glfwTerminate();
-	exit(EXIT_SUCCESS);
-}
-
-void SetupVertexArr(const float& numVAO, GLuint vao[], const float& numVBO, GLuint vbo[], float vArr[], int bIndex) {		// model arr are passed as copy...
-	glGenVertexArrays(numVAO, vao);
-	glBindVertexArray(vao[bIndex]);
-	glGenBuffers(numVBO, vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo[bIndex]);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vArr), vArr, GL_STATIC_DRAW);
-}
-
-void init(GLFWwindow* window, const char* vp, const char* fp, float model[]) {			// not sure if I want to pass everything as args OR set it manually each time...
-	renderingProgram = Utilities::createShaderProgram(vp, fp);
-	// camera setup
-	SetupVertexArr(numVAOs, vao, numVBOs, vbo, model, 0);
-}
-
-void Refresh(void) {
-	glClear(GL_DEPTH_BUFFER);
-	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-	glClear(GL_COLOR_BUFFER_BIT);
-}
-
-glm::mat4 PerspectiveMat(GLFWwindow* window, const float& rad, const float& zNear, const float& zFar) {
-	int width = 0, height = 0;
-	glfwGetFramebufferSize(window, &width, &height);
-	return glm::perspective(rad, (float)width / (float)height, zNear, zFar);
-}
-
-glm::mat4 MvMat(glm::vec3& cam, glm::vec3& model) {
-	return glm::translate(glm::mat4(1.0f), cam) * glm::translate(glm::mat4(1.0f), model);
-}
-
-void display(GLFWwindow* window, const double& currentTime, const char* mv, GLuint& mvLoc, const char* proj, GLuint& projLoc, GLuint& rProg,
-	glm::mat4& mvMat, glm::mat4& perpMat, const float& rad, const float& zNear, const float& zFar, glm::vec3& cam, glm::vec3& model)
-{
-	Refresh();
-	mvLoc = glGetUniformLocation(rProg, mv);
-	projLoc = glGetUniformLocation(rProg, proj);
-	perpMat = PerspectiveMat(window, rad, zNear, zFar);
-	mvMat = MvMat(cam, model);
-
-}
 
 int main(int argc, char* argv) {
-	GLFWwindow* window = CreateWindow(WIDTH, HEIGHT, "Window", MAJOR, MINOR);
+	GLFWwindow* window = Utilities::CreateWindow(WIDTH, HEIGHT, "Window", MAJOR, MINOR);
 	glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK) {
 		std::puts("GLEW lib not initialize");
 		exit(EXIT_FAILURE);
 	}
-	GameLoop(window, "vs.shader", "fg.shader", vertexArr, &init, &display);
-	DestroyWindow(window);
+	Utilities::PreGameLoop(window, renProg, vp, fp, numVAOs, vao, numVBOs, vbo, vertexArr);
+	Utilities::GameLoop(window, glfwGetTime(), mv_matrix, mvLoc, proj_matrix, projLoc, renProg, mvMat, perpMat, glm::radians(60.0f), 0.1f, 1000.0f, cam, model);
+	Utilities::DestroyWindow(window);
 	return 0;
 }
--- Plain_Red_Cube/Plain_Red_Cube/Plain_Red_Cube/Utilities.h
+++ Plain_Red_Cube/Plain_Red_Cube/Plain_Red_Cube/Utilities.h
@@ -5,7 +5,20 @@
 #include "GLSLFileReader.h"
 
 namespace Utilities {
+#if Matrix
+#endif
+	glm::mat4 PerspectiveMat(GLFWwindow* window, const float& rad, const float& zNear, const float& zFar) {
+		int width = 0, height = 0;
+		glfwGetFramebufferSize(window, &width, &height);
+		return glm::perspective(rad, (float)width / (float)height, zNear, zFar);
+	}
 
+	glm::mat4 MvMat(glm::vec3& cam, glm::vec3& model) {
+		return glm::translate(glm::mat4(1.0f), cam) * glm::translate(glm::mat4(1.0f), model);
+	}
+
+#if ShaderProgram
+#endif
 	// Take a vertex Shader and a fragment shader as args
 	GLuint createShaderProgram(const char* vp, const char* fp) {
 		GLint vertCompiled;
@@ -59,15 +72,86 @@ namespace Utilities {
 	// Take a vertex Shader, a geometry shader and a fragment shader as args
 	GLuint createShaderProgram(const char* vp, const char* gp, const char* fp) {
 		// fill in later
+		return 0;
 	}
 
 	// Take a vertex Shader, tessellation and a fragment shader as args
 	GLuint createShaderProgram(const char* vp, const char* tCS, const char* tES, const char* fp) {
 		// fill in later
+		return 0;
 	}
 
 	// Take a vertex Shader, tessellation, a geometry shader and a fragment shader as args
 	GLuint createShaderProgram(const char* vp, const char* tCS, const char* tES, const char* gp, const char* fp) {
 		// fill in later
+		return 0;
+	}
+
+#if main
+#endif
+	GLFWwindow* CreateWindow(const float& width, const float& height, const char* name, const float& major_version, const float& minor_version) {
+		if (!glfwInit()) {
+			std::puts("GLFW lib not initialize");
+			exit(EXIT_FAILURE);
+		}
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, major_version);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, minor_version);
+
+		return glfwCreateWindow(width, height, name, NULL, NULL);
+	}
+
+	void DestroyWindow(GLFWwindow* window) {
+		glfwDestroyWindow(window);
+		glfwTerminate();
+		exit(EXIT_SUCCESS);
+	}
+
+	void SetupVertexArr(const float& numVAO, GLuint vao[], const float& numVBO, GLuint vbo[], float vArr[], int bIndex) {
+		glGenVertexArrays(numVAO, vao);
+		glBindVertexArray(vao[bIndex]);
+		glGenBuffers(numVBO, vbo);
+		glBindBuffer(GL_ARRAY_BUFFER, vbo[bIndex]);
+		glBufferData(GL_ARRAY_BUFFER, sizeof(vArr), vArr, GL_STATIC_DRAW);
+	}
+
+	void init(GLFWwindow* window, GLuint& rProg, const char* vp, const char* fp, const int& numVAOs, GLuint vao[], const int& numVBOs, GLuint vbo[], float model[]) {
+		rProg = Utilities::createShaderProgram(vp, fp);
+		// camera setup
+		SetupVertexArr(numVAOs, vao, numVBOs, vbo, model, 0);
+	}
+
+	void Refresh(GLuint& program) {
+		glClear(GL_DEPTH_BUFFER);
+		glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		glUseProgram(program);
+	}
+
+	void display(GLFWwindow* window, const double& currentTime, const char* mv, GLuint& mvLoc, const char* proj, GLuint& projLoc, GLuint& rProg,
+		glm::mat4& mvMat, glm::mat4& perpMat, const float& rad, const float& zNear, const float& zFar, glm::vec3& cam, glm::vec3& model)
+	{
+		Refresh(rProg);
+		mvLoc = glGetUniformLocation(rProg, mv);
+		projLoc = glGetUniformLocation(rProg, proj);
+		perpMat = PerspectiveMat(window, rad, zNear, zFar);
+		mvMat = MvMat(cam, model);
+		// need to link both matrix to uniform in shader prog
+		// associate VBO with vertex attrib
+		// adjust OpenGL setting / draw model
+	}
+
+	void PreGameLoop(GLFWwindow* window, GLuint& rProg, const char* vp, const char* fp, const int& numVAOs, GLuint vao[], const int& numVBOs, GLuint vbo[], float model[]) {
+		glfwSwapInterval(1);
+		init(window, rProg, vp, fp, numVAOs, vao, numVBOs, vbo, model);
+	}
+
+	void GameLoop(GLFWwindow* window, const double& currentTime, const char* mv, GLuint& mvLoc, const char* proj, GLuint& projLoc, GLuint& rProg,
+		glm::mat4& mvMat, glm::mat4& perpMat, const float& rad, const float& zNear, const float& zFar, glm::vec3& cam, glm::vec3& model)
+	{
+		while (!glfwWindowShouldClose(window)) {
+			display(window, currentTime, mv, mvLoc, proj, projLoc, rProg, mvMat, perpMat, rad, zNear, zFar, cam, model);
+			glfwSwapBuffers(window);
+			glfwPollEvents();
+		}
 	}
 }
