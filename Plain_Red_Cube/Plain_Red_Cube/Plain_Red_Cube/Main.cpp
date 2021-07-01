68676c96ada6cb80e4a6a1536264f578c9bd9388
guyllaume demers <guyllaume_demers@hotmail.fr>
2021-07-01 16:55:28 -0400

    Adding beef inside Init function

--- Plain_Red_Cube/Plain_Red_Cube/Plain_Red_Cube/Main.cpp
+++ Plain_Red_Cube/Plain_Red_Cube/Plain_Red_Cube/Main.cpp
@@ -3,11 +3,21 @@
 #include <GLFW/glfw3.h>
 #include <cstdlib>
 #include <cstdio>
+#include "Utilities.h"
 
 #define WIDTH 600
 #define HEIGHT 400
 #define MAJOR 4
 #define MINOR 3
+
+#define numVAOs 1
+#define numVBOs 2
+
+GLuint renderingProgram;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+float vertexArr[108] = {};
 
 GLFWwindow* CreateWindow(const float& width, const float& height, const char* name, const float& major_version, const float& minor_version) {
 	if (!glfwInit()) {
@@ -36,8 +46,18 @@ void DestroyWindow(GLFWwindow* window) {
 	exit(EXIT_SUCCESS);
 }
 
-void  init(GLFWwindow* window) {
+void SetupVertexArr(const float& numVAO, GLuint vao[], const float& numVBO, GLuint vbo[], float vArr[], int bIndex) {
+	glGenVertexArrays(numVAO, vao);
+	glBindVertexArray(vao[bIndex]);
+	glGenBuffers(numVBO, vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo[bIndex]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vArr), vArr, GL_STATIC_DRAW);
+}
 
+void init(GLFWwindow* window, const char* vp, const char* fp, float model[]) {			// not sure if I want to pass everything as args OR set it manually each time...
+	renderingProgram = Utilities::createShaderProgram(vp, fp);
+	// camera setup
+	SetupVertexArr(numVAOs, vao, numVBOs, vbo, model, 0);
 }
 
 void display(GLFWwindow* window, double currentTime) {
@@ -48,6 +68,7 @@ int main(int argc, char* argv) {
 	GLFWwindow* window = CreateWindow(WIDTH, HEIGHT, "Window", MAJOR, MINOR);
 	glfwMakeContextCurrent(window);
 	if (glewInit() != GLEW_OK) {
+		std::puts("GLEW lib not initialize");
 		exit(EXIT_FAILURE);
 	}
 	GameLoop(window, &init, &display);
