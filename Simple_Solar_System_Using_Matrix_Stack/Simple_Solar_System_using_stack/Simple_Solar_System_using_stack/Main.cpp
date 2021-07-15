a4c6d67554280938cc84be871879d85b1f775386
guyllaume demers <guyllaume_demers@hotmail.fr>
2021-07-15 14:33:22 -0400

    Pushing Simple Solar System using Matrix Stack
    modif on the Utils header.
    -" need to fix models position that are stacked on each other

---
+++ Simple_Solar_System_Using_Matrix_Stack/Simple_Solar_System_using_stack/Simple_Solar_System_using_stack/Main.cpp
@@ -0,0 +1,83 @@
+#pragma once
+#include "Utilities.h"
+
+#define numVAOs 1
+#define numVBOs 2
+
+GLuint rProg;
+GLuint vao[numVAOs];
+GLuint vbo[numVBOs];
+
+GLint mvp_uniform;
+
+const char* mvp_uniform_string = "mvp_matrix";
+
+const char* vp = "vp.shader";
+const char* fp = "fp.shader";
+
+glm::mat4 proj_mat;
+glm::vec3 cam(0.0f, 0.0f, 8.0f);
+
+std::vector<std::vector<float>> models{
+	// Pyramide
+	{
+		 -1.0f, -1.0f, 1.0f, 1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+			1.0f, -1.0f, 1.0f, 1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+			1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+			-1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, 0.0f, 1.0f, 0.0f,
+			-1.0f, -1.0f, -1.0f, 1.0f, -1.0f, 1.0f, -1.0f, -1.0f, 1.0f,
+			1.0f, -1.0f, 1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f
+	},
+	// Cube
+	{
+		-1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f, 1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f, 1.0f,  1.0f, -1.0f, -1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f, 1.0f, -1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f, 1.0f,  1.0f, -1.0f,
+			1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f, -1.0f,  1.0f,  1.0f, 1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f, -1.0f, -1.0f,  1.0f, -1.0f, -1.0f,  1.0f,  1.0f,
+			-1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f, -1.0f,
+			1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f, -1.0f,  1.0f,
+			-1.0f,  1.0f, -1.0f, 1.0f,  1.0f, -1.0f, 1.0f,  1.0f,  1.0f,
+			1.0f,  1.0f,  1.0f, -1.0f,  1.0f,  1.0f, -1.0f,  1.0f, -1.0f
+	}
+};
+
+std::vector<glm::vec3> positions{
+	glm::vec3(0.0f,0.0f,0.0f),																// sun
+	glm::vec3(sin((float)glfwGetTime() * 4.0f), 0.0f, cos((float)glfwGetTime() * 4.0f)),	// earth
+	glm::vec3(0.0f, sin((float)glfwGetTime() * 2.0f), cos((float)glfwGetTime() * 2.0f))		// moon
+};
+
+std::vector<glm::vec3> rotations{
+	glm::vec3(1.0f,0.0f,0.0f),																// sun
+	glm::vec3(0.0f,1.0f,0.0f),																// earth
+	glm::vec3(0.0f,0.0f,1.0f)																// moon
+};
+
+std::vector<glm::vec3> scales{
+	glm::vec3(1.0f,1.0f,1.0f),																// sun
+	glm::vec3(1.0f,1.0f,1.0f),																// earth
+	glm::vec3(0.25f,0.25f,0.25f)															// moon
+};
+
+std::stack<glm::mat4> mvstack;
+
+
+int main(int argc, char* argv) {
+	GLFWwindow* window = Utilities::CreateWindow(600, 400, "Window", 4, 3);
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) {
+		std::puts("GLEW INIT FAILED");
+		exit(EXIT_FAILURE);
+	}
+
+	Utilities::PreGameLoop(window, rProg, vp, fp, proj_mat, glm::radians(60.0f), 0.1f, 1000.0f, models, numVAOs, vao, numVBOs, vbo);
+	Utilities::GameLoop(window, rProg, proj_mat, mvstack, cam, positions, rotations, scales, mvp_uniform, mvp_uniform_string, vbo, models, GL_TRIANGLES);
+	Utilities::DestroyWindow(window);
+
+	return 0;
+}
